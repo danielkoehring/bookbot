7a65d5780902a165d131056c23f1a594804f3f13
danielkoehring <danielkoehring@gmail.com>
2025-09-17 22:47:32 +0200

    add character counting functionality to stats module and update main script

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(path):
     with open(path) as file:
@@ -6,9 +6,12 @@ def get_book_text(path):
 
 
 def main():
-    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    book_text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(book_text)
     print(f"{num_words} words found in the document")
-
+    char_count = count_characters(book_text)
+    print(char_count)
+    
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    new_dict = {}
+
+    for char in text:
+        if char.lower() in new_dict:
+            new_dict[char.lower()] += 1
+        else:
+            new_dict[char.lower()] = 1
+    return new_dict
